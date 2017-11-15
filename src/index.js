68d7c6524af5904a4bd8ad3cc370856fe466b31a
at-dungvan <dung.van@asiantech.vn>
2017-11-15 15:07:11 +0700

    Add inject event plugin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+injectTapEventPlugin();
+
+ReactDOM.render(
+  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
